390951ff70b987d851d0d9ee99ef78f442a72cb5
Edgar0995 <edgar.abgaryan15@gmail.com>
2022-11-07 14:45:30 +0200

    russi

---
+++ js/russ.js
@@ -0,0 +1,11 @@
+const selectos = {
+    popupContainer: '.popup',
+    photo: '.lead__photo',
+}
+
+let popupContainer = document.querySelector('.popup');
+let photo = document.querySelector('.lead__photo');
+
+let popupOpane = function () {
+
+};
